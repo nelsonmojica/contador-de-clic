ee0a2e3d13dc71bcbdd7767ac90c0f567a464b17
NelsonMojica <tecmec777@gmail.com>
2022-08-11 13:21:15 -0600

    hemos aplicado colores a los botones

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Boton from './componentes/Boton';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
+
+    const manejarClic = () => {
+      console.log('Clic');
+    }
+    const reiniciarContador = () => {
+      console.log('Reiniciar');
+    }
+
   return (
     <div className='App'>
       <div className='freecodecamp-logo-contenedor'>
@@ -11,7 +20,14 @@ function App() {
         alt='Logo de freecodecamp'/>
       </div>
       <div className='contenedor-principal'>
-      
+      <Boton
+      texto='Clic'
+      esBotonDeClic={true}
+      manejarClic={manejarClic}/>
+      <Boton 
+      texto='Reiniciar'
+      esBotonDeClic={false}
+      manejarClic={reiniciarContador}/>
       </div>
     </div>
   );
--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../hojas-de-estilo/Boton.css'
 
-function Boton({ texto }) {
+function Boton({ texto, esBotonDeClic, manejarClic }) {
     return(
-        <button>
+        <button
+        className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+        onClick={manejarClic}>
             {texto}
         </button>
     );
 }
+
+export default Boton;
