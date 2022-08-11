3128e01e41e396281271faec9d47dc9d3de25797
NelsonMojica <tecmec777@gmail.com>
2022-08-11 15:25:35 -0600

    se ha usado hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import React from 'react';
 import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import { useState } from 'react';
 
 function App() {
 
+  const [numClics, setNumClics] = useState(0);
+
     const manejarClic = () => {
-      console.log('Clic');
+      setNumClics(numClics + 1);
     }
     const reiniciarContador = () => {
-      console.log('Reiniciar');
+      setNumClics(0);
     }
 
   return (
@@ -21,16 +25,15 @@ function App() {
         alt='Logo de freecodecamp'/>
       </div>
       <div className='contenedor-principal'>
-      <Contador numClics='5'
-      />
+      <Contador numClics={numClics}  />
       <Boton
       texto='Clic'
       esBotonDeClic={true}
-      manejarClic={manejarClic}/>
+      manejarClic={manejarClic} />
       <Boton 
       texto='Reiniciar'
       esBotonDeClic={false}
-      manejarClic={reiniciarContador}/>
+      manejarClic={reiniciarContador} />
       </div>
     </div>
   );
